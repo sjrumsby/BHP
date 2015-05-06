87fb0b54370484f185c90c88506438d7dd5225c7
Sean <sjrumsby@uvic.ca>
2015-05-06 00:46:31 -0700

    Big changes as we move to version 3

---
+++ parsers.py
@@ -0,0 +1,335 @@
+#Contains all of the HTML parsers to pull data from NHL.com
+
+from HTMLParser import HTMLParser
+
+class boxParser(HTMLParser):
+		def __init__(self):
+			HTMLParser.__init__(self)
+			
+			self.rec = 0
+			self.goals_rec = 0
+			self.tr_rec = 0
+			self.goals_tr_rec = 0
+			self.count = 0
+			self.scoringcheck = 0
+			self.ot_check = 0
+			self.prd_span_check = 0
+			self.overtime = 0
+			self.data = []
+			self.goals_data = []
+			self.home_skaters = []
+			self.home_goalies = []
+			self.away_skaters = []
+			self.away_goalies = []
+			
+		def handle_starttag(self, tag, attributes):
+			if tag == "div":
+				for name,value in attributes:
+					if name == "class" and value == "primary":
+						self.scoringcheck = 1
+				for name,value in attributes:
+					if name == "class" and value == "boxData":
+						self.prd_span_check = 1
+			if tag == "table":
+				for name,value in attributes:
+					if name == "class" and value == "stats":
+						self.rec += 1
+					if name == "class" and value == "summary":
+						if self.goals_div:
+							self.goals_rec += 1
+							
+			if tag == "span" and self.prd_span_check:
+				self.ot_check = 1
+
+			if self.rec > 0 and self.rec <= 4:
+				if tag == "tr":
+					self.tr_rec = 1
+				if self.count > 0:
+					for name, value in attributes:
+						if name=='href':
+							id = value.split('=')
+							id = id[1]
+							self.data.append(id)
+			
+			if self.goals_rec > 0:
+				if tag == "tr":
+					self.goals_tr_rec = 1
+			
+			
+		
+		def handle_data(self, data):
+			if self.tr_rec == 1:
+				self.data.append(data)
+				self.count += 1
+			if self.goals_tr_rec:
+				self.data.append(data.strip())
+			if self.scoringcheck:
+				if data == "Scoring Summary":
+					self.goals_div = 1
+			if self.ot_check:
+				self.overtime = 1
+		
+		def handle_endtag(self, tag):
+			if self.rec > 0:
+				if tag == "tr":
+					if self.rec == 1:
+						if "SH TOI" not in self.data and "Saves - Shots" not in self.data:
+							self.away_skaters.append(self.data)
+					elif self.rec == 2:
+						if "SH TOI" not in self.data and "Saves - Shots" not in self.data:
+							self.away_goalies.append(self.data)
+					elif self.rec == 3:
+						if "SH TOI" not in self.data and "Saves - Shots" not in self.data:
+							self.home_skaters.append(self.data)
+					elif self.rec == 4:
+						if "SH TOI" not in self.data and "Saves - Shots" not in self.data:
+							self.home_goalies.append(self.data)
+					self.data = []
+					self.count = 0
+
+				if tag == "table" and self.rec == 4:
+					self.rec += 1
+			
+			if tag == "span" and self.ot_check:
+				self.prd_span_check = 0
+				self.ot_check = 0
+					
+			if self.goals_rec:
+				if tag == "tr":
+					if "Period" not in self.data and "\n\n" not in self.data and "\xc2\xa0" not in self.data and 'NONE' not in self.data and 'OT Period' not in self.data:
+						self.goals_data.append(self.data)
+					self.data = []
+					self.goals_tr_rec = 0
+				
+				if tag == "table":
+					self.goals_rec = 0
+					self.goals_div = 0
+			
+			if self.scoringcheck:
+				self.scoringcheck = 0
+				
+
+class playParser(HTMLParser):
+	def __init__(self):
+		HTMLParser.__init__(self)
+		self.plays 		= []
+		self.home_ice	= []
+		self.away_ice	= []
+		self.play_data 	= []
+		self.t_count 	= 0
+		self.rec 		= 0
+		self.on_ice_rec	= 0
+		self.team		= 0
+		self.td_rec		= 0
+		self.td_count	= 0
+
+	def handle_starttag(self, tag, attributes):
+		if tag == "tr":
+			for name, value in attributes:
+				if name == "class" and value == "evenColor":
+					self.rec = 1
+
+		if tag == "table" and self.rec:
+			self.t_count += 1
+		
+		if tag == "font":
+			self.on_ice_rec = 1
+			
+		if tag == "td" and self.td_rec:
+			self.td_count += 1
+		
+		if tag == "td":
+			for name, value in attributes:
+				if name == "class":
+					if "rborder" in value:
+						self.team = 0
+						self.td_rec = 1
+						self.td_count = 1
+			
+	def handle_data(self, data):
+		if self.rec and self.t_count == 0:
+			if data != '\r\n' and data != '\r\r\n':
+				self.play_data.append(data)
+		
+		if self.on_ice_rec:
+			if self.team == 0:
+				self.away_ice.append(data)
+			else:
+				self.home_ice.append(data)
+
+	def handle_endtag(self, tag):
+		if tag == "table" and self.rec:
+			self.t_count -= 1
+
+		if tag == "tr" and self.rec and self.t_count == 0:
+			if self.play_data != []:
+				if "Elapsed" not in self.play_data and "Description" not in self.play_data:
+					tmp_dict = {'play' : self.play_data, 'home' : self.home_ice, 'away' : self.away_ice }
+					self.plays.append(tmp_dict)
+				self.play_data = []
+				self.home_ice = []
+				self.away_ice = []
+		
+		if tag == "font":
+			self.on_ice_rec = 0
+			
+		if tag == "td" and self.td_rec:
+			self.td_count -= 1
+			
+		if self.td_rec and self.td_count == 0:
+			self.td_rec = 0
+			self.team = 1
+
+class playerParser(HTMLParser):
+	def __init__(self):
+		HTMLParser.__init__(self)
+		self.table = 0
+		self.rec = 0
+		self.player_data = []
+		self.player = []
+		self.count = 0
+
+	def handle_starttag(self, tag, attributes):
+		if tag == "table":
+			for name, value in attributes:
+				if name == "class" and value == "data stats":
+					self.table = 1
+					
+		if self.table:
+			if tag == 'tr':
+				self.rec = 1
+			if self.count == 1:
+				for name, value in attributes:
+					if name=='href':
+						id = value.split('=')
+						id = id[1]
+						self.player_data.append(id)
+
+	def handle_data(self, data):
+		if self.rec:
+			self.player_data.append(data)
+			self.count = self.count + 1
+
+	def handle_endtag(self, tag):
+		if tag == 'tr' and self.rec:
+			if '\nPlayer\n' not in self.player_data and ' | ' not in self.player_data:
+				self.player.append(self.player_data)
+			self.rec = 0
+			self.player_data = []
+			self.count = 0
+		if tag == 'tbody':
+			self.table = 0
+			
+class summaryParser(HTMLParser):
+	def __init__(self):
+		HTMLParser.__init__(self)
+		
+		self.home_team_data = []
+		self.home_rec = 0
+		
+		self.away_team_data = []
+		self.away_rec = 0
+
+		self.summary_rec = 0
+		self.summary_data = []
+		self.append = 0
+
+		self.goal_rec = 0
+		self.goal_row = []
+		self.goal_row_rec = 0
+		self.goal_row_data = []
+
+		self.stars_rec = 0
+		self.stars_row = []
+		self.stars_data = []
+
+	def handle_starttag(self, tag, attributes):
+		if tag == "table":
+			for name,value in attributes:
+				if name == "id" and value == "Visitor":
+					self.away_rec = 1
+			
+				if name =="id" and value == "GameInfo":
+					self.away_rec = 0
+					self.summary_rec = 1
+
+				if name == "id" and value == "Home":
+					self.home_rec = 1
+					self.summary_rec = 0
+		
+		if tag == "tr" and self.goal_rec:
+			self.goal_row_rec = 1
+
+	def handle_data(self, data):
+		if self.home_rec:
+			if data != "" and data != "\r\n" and data != "\r\r\n" and "Match/Game" not in data and "SCORING SUMMARY" not in data:
+				self.home_team_data.append(data)
+		if self.away_rec:
+			if data != "" and data != "\r\n" and data != "\r\r\n" and "Match/Game" not in data and "SCORING SUMMARY" not in data:
+				self.away_team_data.append(data)
+		if self.summary_rec:
+			if data != "\r\n" and data != "\r\r\n" and data != ", " and data != "Sommaire du Match":
+				if self.append:
+					end_row = self.summary_data.pop()
+					end_row += data
+					self.summary_data.append(end_row)
+					self.append = 0
+				else:
+					self.summary_data.append(data)
+		
+		if "SCORING SUMMARY" in data:
+			self.goal_rec = 1
+			
+		if "PENALTY SUMMARY" in data:
+			self.goal_rec = 0
+
+		if self.stars_rec:
+			if data != "\r\n" and data != "\r\r\n":
+				self.stars_row.append(data)
+
+		if "3 STARS" in data:
+			self.stars_rec = 1
+		
+		if self.goal_row_rec:
+			if data != "\r\n" and data != "\r\r\n" and data != ", ":
+				self.goal_row_data.append(data)
+	
+	def handle_endtag(self, tag):
+		if self.stars_rec:
+			if tag == "tr":
+				if self.stars_row != [] and len(self.stars_row) == 4:
+					self.stars_data.append(self.stars_row)
+				self.stars_row = []
+		if self.goal_row_rec and tag == "tr":
+			self.goal_row_rec = 0
+			if "Time" not in self.goal_row_data and len(self.goal_row_data) > 3:
+				self.goal_row.append(self.goal_row_data)
+			self.goal_row_data = []
+				
+	def handle_entityref(self, name):
+		if name == "amp":
+			self.append = 1
+			
+class shootoutParser(HTMLParser):
+	def __init__(self):
+		HTMLParser.__init__(self)
+		self.rec = 0
+		self.shot_data = []
+		self.shots = []
+
+	def handle_starttag(self, tag, attributes):
+		if tag == "tr":
+			for name, value in attributes:
+				if name == "class" and (value == "oddColor + " or value == "evenColor + " or value == "oddColor + bold" or value == "evenColor + bold"):
+					self.rec = 1
+
+	def handle_data(self, data):
+		if self.rec:
+			if data != "\r\n":
+				self.shot_data.append(data)
+
+	def handle_endtag(self, tag):
+		if tag == 'tr' and self.rec:
+			self.shots.append(self.shot_data)
+			self.rec = 0
+			self.shot_data = []
---
+++ vars.py
@@ -0,0 +1,270 @@
+#General Application information
+
+version = "1.0"
+author = "Sean Rumsby"
+database = "data.sqlite"
+debug = 0
+concurrent = 10
+
+#All of the play types and other data required to initialize database
+
+hockey_teams = [
+			(1, "ANA", "ANAHEIM DUCKS"),
+			(2, "BOS", "BOSTON BRUINS"),
+			(3, "BUF", "BUFFALO SABRES"),
+			(4, "CGY", "CALGARY FLAMES"),
+			(5, "CAR", "CAROLINA HURRICANES"),
+			(6, "CHI", "CHICAGO BLACKHAWKS"),
+			(7, "COL", "COLORADO AVALANCHE"),
+			(8, "CBJ", "COLUMBUS BLUE JACKETS"),
+			(9, "DAL", "DALLAS STARS"),
+			(10, "DET", "DETROIT RED WINGS"),
+			(11, "EDM", "EDMONTON OILERS"),
+			(12, "FLA", "FLORIDA PANTHERS"),
+			(13, "LAK", "LOS ANGELES KINGS"),
+			(14, "MIN", "MINNESOTA WILD"),
+			(15, "MTL", "MONTREAL CANADIENS"),
+			(16, "NSH", "NASHVILLE PREDATORS"),
+			(17, "NJD", "NEW JERSEY DEVILS"),
+			(18, "NYI", "NEW YORK ISLANDERS"),
+			(19, "NYR", "NEW YORK RANGERS"),
+			(20, "OTT", "OTTAWA SENATORS"),
+			(21, "PHI", "PHILADELPHIA FLYERS"),
+			(22, "PHX", "PHOENIX COYOTES"),
+			(23, "PIT", "PITTSBURGH PENGUINS"),
+			(24, "SJS", "SAN JOSE SHARKS"),
+			(25, "STL", "ST. LOUIS BLUES"),
+			(26, "TBL", "TAMPA BAY LIGHTNING"),
+			(27, "TOR", "TORONTO MAPLE LEAFS"),
+			(28, "VAN", "VANCOUVER CANUCKS"),
+			(29, "WSH", "WASHINGTON CAPITALS"),
+			(30, "WPG", "WINNIPEG JETS")
+		]
+		
+miss_types 	= [
+				(1, "Crossbar"), 
+				(2, "Over"), 
+				(3, "Post"), 
+				(4, "Tip"),
+				(5, "Wide"),
+				]
+				
+penl_types = [
+				(1, "Boarding"),
+				(2, "Broken stick"),
+				(3, "Charging"),
+				(4, "Clipping"),
+				(5, "Closing hand on the puck"),
+				(6, "Cross-checking"),
+				(7, "Delay of game"),
+				(8, "Elbowing"),
+				(9, "Goalkeeper interference"),
+				(10, "High-sticking"),
+				(11, "Holding"),
+				(12, "Holding the stick"),
+				(13, "Hooking"),
+				(14, "Illegal equipment"),
+				(15, "Illegal stick"),
+				(16, "Instigator"),
+				(17, "Interference"),
+				(18, "Kneeing"),
+				(19, "Leaving penalty bench too early"),
+				(20, "Leaving the crease (goalkeeper)"),
+				(21, "Participating in the play beyond the center red line (goalkeeper)"),
+				(22, "Roughing"),
+				(23,"Slashing"),
+				(24, "Throwing puck towards opponent's goal (goalkeeper)"),
+				(25, "Throwing stick"),
+				(26, "Tripping"),
+				(27, "Unsportsmanlike conduct"),
+				]
+
+play_types 	= [
+				(1, "BLOCK"), 
+				(2, "FAC"),
+				(3, "GIVE"),
+				(4, "GOAL"),
+				(5, "GEND"),
+				(6, "HIT"),
+				(7, "MISS"),
+				(8, "PEND"),
+				(9, "PENL"),
+				(10, "PSTR"),
+				(11, "SHOT"),
+				(12, "STOP"),
+				(13, "TAKE"),
+				(14, "SOC"),
+				(15, "EISTR"),
+				(16, "EIEND"),
+				(17, "GOFF"),
+				]
+
+shot_types 	= [
+				(1, "Backhand"),
+				(2, "Snap"),
+				(3,"Slap"),
+				(4, "Tip"),
+				(5, "Wrap"),
+				(6, "Wrist"),
+				(7, "Deflected"),
+				(8, "Tip-In"),
+				(9, "Wrap-around"),
+				(10, "Undefined"),
+				]
+
+stop_types 	= [
+				(1, "Frozen"),
+				(2, "Goalie"),
+				(3, "Icing"),
+				(4, "Hand Pass"),
+				(5, "High Stick"), 
+				(6, "Injury"),
+				(7, "In Benches"), 
+				(8, "In Crowd"),
+				(9, "In Netting"), 
+				(10, "Net Off"),
+				(11, "TV Timeout"),
+				(12, "Official Injury"),
+				(13, "Offside"),
+				(14, "Referee or Linesman"),
+				(15, "Player Equipment"),
+				(16, "Visitor Timeout"),
+				(17, "Home Timeout"),
+				(18, "Clock Problem"),
+				(19, "Video Review"),
+				(20, "Rink Repair"),
+				]
+
+strength_types = [
+					(1, "SH"),
+					(2, "EV"),
+					(3, "PP"),
+				]
+				
+zone_types 	= [
+				(1, "Offensive"), 
+				(2, "Neutral"),
+				(3, "Defensive"),
+				]
+				
+miss_types_reverse = dict((v,k) for k,v in miss_types)
+penl_types_reverse = dict((v,k) for k,v in penl_types)
+play_types_reverse = dict((v,k) for k,v in play_types)
+shot_types_reverse = dict((v,k) for k,v in shot_types)
+stop_types_reverse = dict((v,k) for k,v in stop_types)
+zone_types_reverse = dict((v,k) for k,v in zone_types)
+strength_types_reverse = dict((v,k) for k,v in strength_types)
+
+#General data structures useful throughout Application
+
+shortNameToID = {	"ANA" : 1,
+					"ARI" : 22,
+					"BOS" : 2,
+					"BUF" : 3, 
+					"CGY" : 4,
+					"CAR" : 5,
+					"CHI" : 6,
+					"COL" : 7,
+					"CBJ" : 8,
+					"DAL" : 9,
+					"DET" : 10,
+					"EDM" : 11,
+					"FLA" : 12,
+					"LAK" : 13,
+					"MIN" : 14,
+					"MTL" : 15,
+					"NSH" : 16,
+					"NJD" : 17,
+					"NYI" : 18,
+					"NYR" : 19,
+					"OTT" : 20,
+					"PHI" : 21,
+					"PHX" : 22,
+					"PIT" : 23,
+					"SJS" : 24,
+					"STL" : 25,
+					"TBL" : 26,
+					"TOR" : 27,
+					"VAN" : 28,
+					"WSH" : 29,
+					"WPG" : 30,
+					"T.B" : 26,
+					"L.A" : 13,
+					"N.J" : 17
+				}
+
+longNameToID = {	"ANAHEIM DUCKS" : 1,
+					"ARIZONA COYOTES" : 22,
+					"BOSTON BRUINS" : 2,
+					"BUFFALO SABRES" : 3,
+					"CALGARY FLAMES" : 4,
+					"CAROLINA HURRICANES" : 5,
+					"CHICAGO BLACKHAWKS" : 6,
+					"COLORADO AVALANCHE" : 7,
+					"COLUMBUS BLUE JACKETS" : 8,
+					"DALLAS STARS" : 9,
+					"DETROIT RED WINGS" : 10,
+					"EDMONTON OILERS" : 11,
+					"FLORIDA PANTHERS" : 12,
+					"LOS ANGELES KINGS" : 13,
+					"MINNESOTA WILD" : 14,
+					"MONTREAL CANADIENS" : 15,
+					"NASHVILLE PREDATORS" : 16,
+					"NEW JERSEY DEVILS" : 17,
+					"NEW YORK ISLANDERS" : 18,
+					"NEW YORK RANGERS" : 19,
+					"OTTAWA SENATORS" : 20,
+					"PHILADELPHIA FLYERS" : 21,
+					"PHOENIX COYOTES" : 22,
+					"PITTSBURGH PENGUINS" : 23,
+					"SAN JOSE SHARKS" : 24,
+					"ST. LOUIS BLUES" : 25,
+					"TAMPA BAY LIGHTNING" : 26,
+					"TORONTO MAPLE LEAFS" : 27,
+					"VANCOUVER CANUCKS" : 28,
+					"WASHINGTON CAPITALS" : 29,
+					"WINNIPEG JETS" : 30,
+				}
+				
+
+longNameToShortName = {	"ANAHEIM DUCKS" : "ANA",
+						"ARIZONA COYOTES" : "ARI",
+						"BOSTON BRUINS" : "BOS",
+						"BUFFALO SABRES" : "BUF",
+						"CALGARY FLAMES" : "CGY",
+						"CAROLINA HURRICANES" : "CAR",
+						"CHICAGO BLACKHAWKS" : "CHI",
+						"COLORADO AVALANCHE" : "COL",
+						"COLUMBUS BLUE JACKETS" : "CBJ",
+						"DALLAS STARS" : "DAL",
+						"DETROIT RED WINGS" : "DET",
+						"EDMONTON OILERS" : "EDM",
+						"FLORIDA PANTHERS" : "FLA",
+						"LOS ANGELES KINGS" : "LAK",
+						"MINNESOTA WILD" : "MIN",
+						"MONTREAL CANADIENS" : "MTL",
+						"NASHVILLE PREDATORS" : "NSH",
+						"NEW JERSEY DEVILS" : "NJD",
+						"NEW YORK ISLANDERS" : "NYI",
+						"NEW YORK RANGERS" : "NYR",
+						"OTTAWA SENATORS" : "OTT",
+						"PHILADELPHIA FLYERS" : "PHI",
+						"PHOENIX COYOTES" : "PHX",
+						"PITTSBURGH PENGUINS" : "PIT",
+						"SAN JOSE SHARKS" : "SJS",
+						"ST. LOUIS BLUES" : "STL",
+						"TAMPA BAY LIGHTNING" : "TBL",
+						"TORONTO MAPLE LEAFS" : "TOR",
+						"VANCOUVER CANUCKS" : "VAN",
+						"WASHINGTON CAPITALS" : "WSH",
+						"WINNIPEG JETS" : "WPG",
+					}
+
+idToShortName = dict((v,k) for k,v in shortNameToID.iteritems())
+idToLongName = dict((v,k) for k,v in longNameToID.iteritems())
+shortNameToLongName = dict((v,k) for k,v in longNameToShortName.iteritems())
+
+numberOfGames = { 	"20112012" : 1230,
+					"20122013" : 624,
+					"20132014" : 1230,
+				}
